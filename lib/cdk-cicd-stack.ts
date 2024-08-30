b96021249607ac6b7102f0ff21a41172bbcddced
AlexanderJumbo <geossephyjumboalava@gmail.com>
2024-08-30 16:31:45 -0500

    fix the error

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwesomePipeline',{
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('AlexanderJumbo/cdk-cicd','master'), // especificamos de donde este pipeline tomará el código fuente
+        commands: [
+          //'cd cdk-cicd', solo en caso de que el repo sea un folder que contenga varios otros repos
+          'npm ci',
+          'npx cdk synth'
+        ],
+        //primaryOutputDirectory: 'cdk-cicd/cdk.out' solo en caso de que el repo sea un folder que contenga varios otros repos
+      })
+    })
 
   }
 }
